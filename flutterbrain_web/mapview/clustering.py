f1f479bd280144fc066fee29a63ceb7e002e5a87
Uwe Kamper <ukamper@itfm.de>
2015-12-05 23:19:42 +0100

    tests for clustering

--- flutterbrain_web/mapview/clustering.py
+++ flutterbrain_web/mapview/clustering.py
@@ -1,12 +1,8 @@
-# from pyspark.mllib.clustering import KMeans, KMeansModel
-# data = [[10,20,30,40],
-# [1,-1,0,2],
-# [10,21,31,40],
-# [0,0,0,0]]
-# rdd = sc.parallelize(data)
-# rdd.collect()
-# clusters = KMeans.train(rdd, 2, maxIterations=10,
-#         runs=10, initializationMode="random")
-# print clusters.centers
-# print(clusters.predict([10,20,20,20]))
-# print(clusters.predict([0,0,0,0]))#
+# -*- coding: utf-8 -*-
+import requests
+
+def get_tweets(hashtag):
+    url = u'http://loklak.org:9000/api/search.json?q={}&source=twitter&minified=true'.format(hashtag)
+    resp = requests.get(url)
+    data = resp.json()
+    return data
--- flutterbrain_web/mapview/tests.py
+++ flutterbrain_web/mapview/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 
+from .clustering import get_tweets
+
 # Create your tests here.
 class MapViewTests(TestCase):
-
-    def test_mapview_index(self):
-        pass
+    """
+    TODO: Document the shit out of this class.
+    """
+    def test_get_tweets(self):
+        """
+        This is the test_get_tweets method. It tests get_tweets()!
+        """
+        tweets = get_tweets('flutterbrn')
+        print tweets
